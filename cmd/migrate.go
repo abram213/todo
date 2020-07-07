8ce41375ab0cae5d795d8b5ffb9090e0a6b9521c
abram213 <abram13504@yandex.ru>
2020-07-07 17:38:43 +0300

    Added migrate with refresh flag command, to migrate or drop and migrate tables in sql data mode.

---
+++ cmd/migrate.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"todo/app"
+	"todo/model"
+)
+
+var migrateCmd = &cobra.Command{
+	Use: "migrate",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		refresh, _ := cmd.Flags().GetBool("refresh")
+
+		a, err := app.New()
+		if err != nil {
+			return err
+		}
+		defer a.Database.CloseDB()
+
+		if refresh {
+			fmt.Println("dropping tables...")
+			a.Database.DropTables(
+				&model.Todo{})
+		}
+
+		a.Database.Migrate(
+			&model.Todo{})
+		fmt.Println("migration success!")
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(migrateCmd)
+	migrateCmd.Flags().Bool("refresh", false, "drop all tables before migration")
+}
